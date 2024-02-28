08134a4bbd0b1e474de90182c0b384a859e60275
Humayun Habib <humayunhabib786@gmail.com>
2024-02-28 18:11:10 +0500

    handling image

---
+++ src/popup/popup.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import "../assets/tailwind.css";
+
+const test = (
+  <div>
+    <h1 className="text-4xl text-green-500">hello World</h1>
+    <p>lorem epsom</p>
+  </div>
+);
+
+const container = document.createElement("div");
+
+document.body.appendChild(container);
+const root = createRoot(container);
+root.render(test);
